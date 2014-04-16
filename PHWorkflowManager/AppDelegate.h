e9e388fdec3bfb9cc262c498164a4839fac8babf
Jimmy Hough Jr <jimmyhoughjr@me.com>
2014-04-16 18:52:19 -0500

    Created project

---
+++ PHWorkflowManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PHWorkflowManager
+//
+//  Created by Jimmy Hough Jr on 4/16/14.
+//  Copyright (c) 2014 PH Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PHWorkflowManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PHWorkflowManager
+//
+//  Created by Jimmy Hough Jr on 4/16/14.
+//  Copyright (c) 2014 PH Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
